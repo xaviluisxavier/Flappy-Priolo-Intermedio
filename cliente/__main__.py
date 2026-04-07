c11628effae9a610a30df376554339bd592146c9
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-04-07 21:50:55 +0000

    Update __main__.py

--- cliente/__main__.py
+++ cliente/__main__.py
@@ -2,7 +2,6 @@ import sys
 from comunicacao.cliente_jogo import ClientePriolo
 
 if __name__ == '__main__':
-    # Permite passar o IP do servidor no terminal. Se não passares nada, usa localhost.
     ip_servidor = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
     cliente = ClientePriolo(host=ip_servidor)
     cliente.jogar()
