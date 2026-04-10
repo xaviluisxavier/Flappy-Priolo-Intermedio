70cbe849517b6b74e13e3a073ad4b5831f6656ac
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-04-10 11:40:54 +0000

    Refactor broadcast receiver for improved clarity

--- cliente/broadcast_receiver.py
+++ cliente/broadcast_receiver.py
@@ -27,32 +27,41 @@ class BroadcastReceiver(threading.Thread):
                 os.system('cls' if os.name == 'nt' else 'clear')
                 print("FLAPPY PRIOLO")
                 
-                # Para CADA jogador
+               # Para CADA jogador no dicionário
                 for pid, info in estado['jogadores'].items(): 
                     
                     print(f"\n[Ecrã de {info['nome']}] Pontos: {info['score']}")
-                    print("=" * 51) 
+                    print("=" * 51)
                     
-                    # Desenha o jogo
+                    # ecrã de cima para baixo (Eixo Y)
                     for y in range(10, 100, 10): 
                         linha = ""
-                        for x in range(0, 101, 2): 
-                            char = " " 
-                            
-                            # Desenha os vulcões
-                            for v in estado['vulcoes']:
-                                if abs(v['x'] - x) <= 2:
-                                    if y < v['abertura_y'] - 15 or y > v['abertura_y'] + 15:
-                                        char = "X" 
-                            
-                            # Desenha o pássaro
-                            if info['x'] == x and abs(info['y'] - y) <= 4:
-                                char = "🐦" 
-                                    
-                            linha += char
-                        print(linha)
                         
-                    print("=" * 51)
+                        # linha da esquerda para a direita (Eixo X)
+                        for x in range(0, 101, 2): 
+                            pixel = " "
+                            
+                            # 1. Verifica se nesta coordenada exata (X, Y) existe um VULCÃO
+                            for vulcao in estado['vulcoes']:
+                                na_coluna_do_vulcao = abs(vulcao['x'] - x) <= 2
+                                bate_no_vulcao = y < vulcao['abertura_y'] - 15 or y > vulcao['abertura_y'] + 15
+                                
+                                if na_coluna_do_vulcao and bate_no_vulcao:
+                                    pixel = "X" 
+                            
+                            # 2. Verifica se nesta coordenada exata (X, Y) está o PÁSSARO
+                            no_x_do_passaro = (info['x'] == x)
+                            no_y_do_passaro = abs(info['y'] - y) <= 4
+                            
+                            if no_x_do_passaro and no_y_do_passaro:
+                                pixel = "🐦" 
+                                    
+                            # Adiciona o pixel (espaço, 'X' ou '🐦') à linha
+                            linha += pixel 
+                            
+                        # Imprime a linha no terminal
+                        print(linha)
+                    print("=" * 51) 
                     
                 # Mensagem final no fundo do terminal
                 print("\nEscreve 'f' e [ENTER] para saltar!")
