eb6965395bc3584b2d54891ce704b205b2449c2f
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-04-23 11:23:21 +0000

    Delete cliente directory

--- cliente/__main__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from cliente.interface.cliente_jogo import ClientePriolo
-
-if __name__ == '__main__':
-    ip_servidor = sys.argv[1] if len(sys.argv) > 1 else '127.0.0.1'
-    cliente = ClientePriolo()     
-    cliente.jogar(host=ip_servidor)
