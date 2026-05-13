5534a6f77d389b82c42b27d49877e3643ede00e2
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-05-13 13:49:31 +0000

    Delete cliente directory

--- cliente/__main__.py
+++
@@ -1,9 +0,0 @@
-from cliente.interface.interface import Interface
-
-def main():
-    print("A iniciar o cliente Flappy Priolo.")
-    interface_cliente = Interface()
-    interface_cliente.execute()
-
-if __name__ == '__main__':
-    main()
